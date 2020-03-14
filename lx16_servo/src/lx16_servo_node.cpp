e0bc03bb87282e69be535172aae352849b68b29c
at91sam7a3 <at91sam7a3@gmail.com>
2020-03-14 12:09:52 +0200

    lx16 node

--- lx16_servo/src/lx16_servo_node.cpp
+++ lx16_servo/src/lx16_servo_node.cpp
@@ -1,17 +1,31 @@
 #include <ros/ros.h>
 #include "lx16_servo/Servo.h"
 #include "std_msgs/String.h"
+#include "../lx16lib/lx16driver.h"
+#include "lx16_servo_node.h"
 
-void servo_pos_callback(const lx16_servo::Servo::ConstPtr msg)
-{
-    ROS_INFO("Set Servo ", msg->Id, " to position ", msg->Position);
-}
+
 
 int main(int argc, char** argv)
 {
     ros::init(argc,argv,"lx16_servo");
-    ros::NodeHandle nh;
-    ros::Subscriber sub = nh.subscribe("servo_pos", 100, servo_pos_callback);
+    Lx16ServoNode servoNode;
     ros::spin();
     return 0;
+}
+
+Lx16ServoNode::Lx16ServoNode()
+    :m_nh()
+    ,m_sub(m_nh.subscribe("servo_pos", 100, &Lx16ServoNode::servoPosCallback,this))
+    ,m_driver("/dev/serial0",true)
+{
+    if( m_driver.isOperational() == false ){
+        ROS_ERROR("Serial /dev/serial0 not available");
+    }
+}
+
+void Lx16ServoNode::servoPosCallback(const lx16_servo::Servo::ConstPtr msg)
+{
+    ROS_INFO("Set Servo ", msg->Id, " to position ", msg->Position);
+    m_driver.ServoMoveTimeWrite(msg->Id,msg->Position,100);
 }
---
+++ lx16_servo/src/lx16_servo_node.h
@@ -0,0 +1,21 @@
+#ifndef LX16_SERVO_NODE_H
+#define LX16_SERVO_NODE_H
+
+
+class Lx16ServoNode
+{
+public:
+    Lx16ServoNode();
+    ~Lx16ServoNode() = default;
+
+private:
+    void servoPosCallback(const lx16_servo::Servo::ConstPtr msg);
+
+private:
+    ros::NodeHandle m_nh;
+    ros::Subscriber m_sub;
+    lx16driver m_driver;
+};
+
+
+#endif // LX16_SERVO_NODE_H
