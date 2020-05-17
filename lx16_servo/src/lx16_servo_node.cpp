22ee99d9eadc9c11963dd8b8c2c846060b44673d
at91sam7a3 <at91sam7a3@gmail.com>
2020-05-17 19:12:38 +0300

    refactoring

--- lx16_servo/src/lx16_servo_node.cpp
+++ lx16_servo/src/lx16_servo_node.cpp
@@ -1,31 +1,59 @@
 #include <ros/ros.h>
 #include "../../../devel/include/spider_messages/Servo.h"
+#include "../../../devel/include/spider_messages/LegPower.h"
 #include "std_msgs/String.h"
 #include "../lx16lib/lx16driver.h"
 #include "lx16_servo_node.h"
+#include <wiringPi.h>
+#include <vector>
 
+struct servoCommand
+{
+    int id;
+    int position;
+};
 
+std::vector<servoCommand> commands;
+
+void powerSwitchCallback(const spider_messages::LegPowerConstPtr msg)
+{
+    bool on = msg->PowerToLegs;
+    digitalWrite (7,on);
+}
+
+void servoPosCallback(const spider_messages::Servo::ConstPtr msg)
+{
+    ROS_INFO("Set Servo ", msg->Id, " to position ", msg->Position);
+    servoCommand c = {msg->Id,msg->Position};
+    commands.push_back(c);
+}
 
 int main(int argc, char** argv)
 {
-    ros::init(argc,argv,"lx16_servo");
-    Lx16ServoNode servoNode;
-    ros::spin();
-    return 0;
-}
+    wiringPiSetup ();
+    pinMode (7,OUTPUT); //Turn on power relay
 
-Lx16ServoNode::Lx16ServoNode()
-    :m_nh()
-    ,m_sub(m_nh.subscribe("servo_pos", 100, &Lx16ServoNode::servoPosCallback,this))
-    ,m_driver("/dev/serial0",true)
-{
+    ros::init(argc,argv,"lx16_servo");
+    ros::NodeHandle m_nh;
+    ros::Subscriber power_sub = m_nh.subscribe("servo_power", 100, &powerSwitchCallback);
+    ros::Subscriber position_sub =  m_nh.subscribe("servo_pos", 100, &servoPosCallback);
+
+    lx16driver m_driver("/dev/serial0",true);
     if( m_driver.isOperational() == false ){
         ROS_ERROR("Serial /dev/serial0 not available");
     }
-}
 
-void Lx16ServoNode::servoPosCallback(const spider_messages::Servo::ConstPtr msg)
-{
-    ROS_INFO("Set Servo ", msg->Id, " to position ", msg->Position);
-    m_driver.ServoMoveTimeWrite(msg->Id,msg->Position,100);
+    ros::Rate r(100); // 10 hz
+    while (ros::ok())
+    {
+        if(commands.size() > 0){
+           servoCommand s = commands[size-1];
+           commands.pop_back();
+           m_driver.ServoMoveTimeWrite(s.id,s.position,100);
+        }
+        ros::spinOnce();
+        r.sleep();
+    }
+
+    return 0;
 }
