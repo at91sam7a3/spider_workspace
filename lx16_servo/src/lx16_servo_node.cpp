a1d799058ec3ae901b47622955aa5bd4f1a5ebc2
at91sam7a3 <at91sam7a3@gmail.com>
2020-05-17 20:41:33 +0100

    finally works

--- lx16_servo/src/lx16_servo_node.cpp
+++ lx16_servo/src/lx16_servo_node.cpp
@@ -4,15 +4,10 @@
 #include "std_msgs/String.h"
 #include "../lx16lib/lx16driver.h"
 #include <wiringPi.h>
-#include <vector>
 
-struct servoCommand
-{
-    int id;
-    int position;
-};
+lx16driver m_driver("/dev/serial0",true);
 
-std::vector<servoCommand> commands;
+
 
 void powerSwitchCallback(const spider_messages::LegPowerConstPtr msg)
 {
@@ -22,35 +17,33 @@ void powerSwitchCallback(const spider_messages::LegPowerConstPtr msg)
 
 void servoPosCallback(const spider_messages::Servo::ConstPtr msg)
 {
-    ROS_INFO("Set Servo ", msg->Id, " to position ", msg->Position);
-    servoCommand c = {msg->Id,msg->Position};
-    commands.push_back(c);
+    m_driver.ServoMoveTimeWrite(msg->Id,msg->Position,100);
 }
 
 int main(int argc, char** argv)
 {
+    ros::init(argc,argv,"lx16_servo");
+
+    ros::NodeHandle m_nh;
+
+    ros::Duration(0.1).sleep();
+
     wiringPiSetup ();
     pinMode (7,OUTPUT); //Turn on power relay
-
-    ros::init(argc,argv,"lx16_servo");
-    ros::NodeHandle m_nh;
-    ros::Subscriber power_sub = m_nh.subscribe("servo_power", 100, &powerSwitchCallback);
-    ros::Subscriber position_sub =  m_nh.subscribe("servo_pos", 100, &servoPosCallback);
     digitalWrite (7,true);
-    lx16driver m_driver("/dev/serial0",true);
+    ros::Duration(0.1).sleep();
     if( m_driver.isOperational() == false ){
         ROS_ERROR("Serial /dev/serial0 not available");
     }
+    m_driver.ServoVoltageRead(0);
 
+
+
+    ros::Subscriber power_sub = m_nh.subscribe("servo_power", 100, &powerSwitchCallback);
+    ros::Subscriber position_sub =  m_nh.subscribe("servo_pos", 100, &servoPosCallback);
     ros::Rate r(100); // 100 hz
     while (ros::ok())
     {
-        if(commands.size() > 0)
-        {
-            servoCommand s = commands[commands.size()-1];
-            commands.pop_back();
-            m_driver.ServoMoveTimeWrite(s.id,s.position,100);
-        }
         ros::spinOnce();
         r.sleep();
     }
